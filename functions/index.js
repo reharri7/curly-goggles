539a73a0b2d902991e30e2fffcb68633248d8d01
Rhett Harrison <reharri7@asu.edu>
2021-06-10 07:02:48 -0700

    fixed linting error in index.js

--- functions/index.js
+++ functions/index.js
@@ -14,7 +14,7 @@ admin.initializeApp(functions.config().firebase);
 const app = express();
 const main = express();
 
-// add the path to receive request and set json as bodyParser to process the body 
+// add the path to receive request and set json as bodyParser to process the body
 main.use("/api/v1", app);
 main.use(bodyParser.json());
 main.use(bodyParser.urlencoded({extended: false}));
